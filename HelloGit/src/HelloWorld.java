d8d1d7872c334c06011a91a545bddeb9f6bb767b
lks61716 <lgs61716@gmail.com>
2021-08-26 11:07:33 +0900

    Update HelloWorld.java
    from git

--- HelloGit/src/HelloWorld.java
+++ HelloGit/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
 		System.out.println("Hello World2222");
+		System.out.println("Hello 123123World2222");
 	}
 }
